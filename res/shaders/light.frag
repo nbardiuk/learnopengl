106cc5a62aa888946684318162ab6e3bba84da68
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-11-17 09:05:13 +0000

    Diffuse map

--- res/shaders/light.frag
+++ res/shaders/light.frag
@@ -1,8 +1,7 @@
 #version 330 core
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
+    sampler2D diffuse;
     vec3 specular;
     float shininess;
 };
@@ -18,18 +17,19 @@ uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
 
+in vec2 TexCoords;
 in vec3 FragPos;
 in vec3 Normal;
 
 out vec4 FragColor;
 
 void main() {
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,18 @@ use cgmath::Point2;
 use gl::types::GLfloat;
 use gl::types::GLint;
 use gl::types::GLsizeiptr;
+use gl::types::GLuint;
 use glfw::Action;
 use glfw::Context;
 use glfw::Key;
 use glfw::Window;
 use glfw::WindowEvent;
 use glfw::WindowHint;
+use image::ColorType;
+use image::GenericImageView;
 use std::mem;
 use std::os::raw::c_void;
+use std::path::Path;
 use std::ptr;
 use std::sync::mpsc::Receiver;
 
@@ -50,45 +54,52 @@ fn main() {
     let lamp_shader = Shader::new("res/shaders/lamp.vert", "res/shaders/lamp.frag")
         .expect("Shader Program linkage failed");
 
-    let vertices: [GLfloat; 216] = [
-        // position       // normal
-        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, //
-        0.5, -0.5, -0.5, 0.0, 0.0, -1.0, //
-        0.5, 0.5, -0.5, 0.0, 0.0, -1.0, //
-        0.5, 0.5, -0.5, 0.0, 0.0, -1.0, //
-        -0.5, 0.5, -0.5, 0.0, 0.0, -1.0, //
-        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, //
-        -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, //
-        0.5, -0.5, 0.5, 0.0, 0.0, 1.0, //
-        0.5, 0.5, 0.5, 0.0, 0.0, 1.0, //
-        0.5, 0.5, 0.5, 0.0, 0.0, 1.0, //
-        -0.5, 0.5, 0.5, 0.0, 0.0, 1.0, //
-        -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, //
-        -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, //
-        -0.5, 0.5, -0.5, -1.0, 0.0, 0.0, //
-        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, //
-        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, //
-        -0.5, -0.5, 0.5, -1.0, 0.0, 0.0, //
-        -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, //
-        0.5, 0.5, 0.5, 1.0, 0.0, 0.0, //
-        0.5, 0.5, -0.5, 1.0, 0.0, 0.0, //
-        0.5, -0.5, -0.5, 1.0, 0.0, 0.0, //
-        0.5, -0.5, -0.5, 1.0, 0.0, 0.0, //
-        0.5, -0.5, 0.5, 1.0, 0.0, 0.0, //
-        0.5, 0.5, 0.5, 1.0, 0.0, 0.0, //
-        -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, //
-        0.5, -0.5, -0.5, 0.0, -1.0, 0.0, //
-        0.5, -0.5, 0.5, 0.0, -1.0, 0.0, //
-        0.5, -0.5, 0.5, 0.0, -1.0, 0.0, //
-        -0.5, -0.5, 0.5, 0.0, -1.0, 0.0, //
-        -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, //
-        -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, //
-        0.5, 0.5, -0.5, 0.0, 1.0, 0.0, //
-        0.5, 0.5, 0.5, 0.0, 1.0, 0.0, //
-        0.5, 0.5, 0.5, 0.0, 1.0, 0.0, //
-        -0.5, 0.5, 0.5, 0.0, 1.0, 0.0, //
-        -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, //
+    let vertices: [f32; 288] = [
+        // positions      // normals      // texture coords
+        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0, //
+        0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 0.0, //
+        0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0, //
+        0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0, //
+        -0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 1.0, //
+        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0, //
+        //
+        -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, //
+        0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 0.0, //
+        0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0, //
+        0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0, //
+        -0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0, //
+        -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, //
+        //
+        -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0, 0.0, //
+        -0.5, 0.5, -0.5, -1.0, 0.0, 0.0, 1.0, 1.0, //
+        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0, //
+        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0, //
+        -0.5, -0.5, 0.5, -1.0, 0.0, 0.0, 0.0, 0.0, //
+        -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0, 0.0, //
+        //
+        0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0, //
+        0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 1.0, 1.0, //
+        0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 1.0, //
+        0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 1.0, //
+        0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 0.0, //
+        0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0, //
+        //
+        -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0, //
+        0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 1.0, 1.0, //
+        0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0, 0.0, //
+        0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0, 0.0, //
+        -0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 0.0, 0.0, //
+        -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0, //
+        //
+        -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, //
+        0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 1.0, //
+        0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0, //
+        0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0, //
+        -0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0, //
+        -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, //
     ];
+
+    let diffuse_map = load_texture("res/textures/container2.png").unwrap();
 
     let mut cube_vao = 0;
     let mut lamp_vao = 0;
@@ -101,7 +112,9 @@ fn main() {
         let vert_size = mem::size_of_val(&vertices) as GLsizeiptr;
         gl::BufferData(gl::ARRAY_BUFFER, vert_size, vert_ref, gl::STATIC_DRAW);
 
-        let stride = 6 * mem::size_of::<GLfloat>() as GLint;
+        let floats = |n: usize| (n * mem::size_of::<GLfloat>()) as GLint;
+        let stride = floats(8);
+        let void = |n| n as *const c_void;
 
         // 2. bind Vertex Array Object for lamp
         gl::GenVertexArrays(1, &mut lamp_vao);
@@ -116,15 +129,11 @@ fn main() {
         gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
         gl::EnableVertexAttribArray(0);
         // aNormal
-        gl::VertexAttribPointer(
-            1,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            stride,
-            (3 * mem::size_of::<GLfloat>()) as *const c_void,
-        );
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, stride, void(floats(3)));
         gl::EnableVertexAttribArray(1);
+        // aTexCoords
+        gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, stride, void(floats(6)));
+        gl::EnableVertexAttribArray(2);
     }
 
     unsafe {
@@ -163,23 +172,20 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
             let light_pos = vec3(1.2, 1.0, 2.0);
-            let light_color = vec3(
-                (glfw.get_time() * 2.).sin() as f32,
-                (glfw.get_time() * 0.7).sin() as f32,
-                (glfw.get_time() * 1.3).sin() as f32,
-            );
-            let diffuse = light_color * 0.5;
-            let ambient = diffuse * 0.2;
 
             light_shader.use_program();
             light_shader.set_vec3("light.position", light_pos);
-            light_shader.set_vec3("light.ambient", ambient);
-            light_shader.set_vec3("light.diffuse", diffuse);
-            light_shader.set_vec3("light.specular", vec3(1., 1., 1.));
+            light_shader.set_vec3("light.ambient", vec3(0.2, 0.2, 0.2));
+            light_shader.set_vec3("light.diffuse", vec3(0.5, 0.5, 0.5));
+            light_shader.set_vec3("light.specular", vec3(1.0, 1.0, 1.0));
+
             light_shader.set_vec3("material.ambient", vec3(1., 0.5, 0.31));
-            light_shader.set_vec3("material.diffuse", vec3(1., 0.5, 0.31));
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_2D, diffuse_map);
+            light_shader.set_int("material.diffuse", 0);
             light_shader.set_vec3("material.specular", vec3(0.5, 0.5, 0.5));
             light_shader.set_float("material.shininess", 32.);
+
             light_shader.set_vec3(
                 "viewPos",
                 vec3(camera.position.x, camera.position.y, camera.position.z),
@@ -209,6 +215,7 @@ fn main() {
         gl::DeleteVertexArrays(1, &cube_vao);
         gl::DeleteVertexArrays(1, &lamp_vao);
         gl::DeleteBuffers(1, &vbo);
+        gl::DeleteTextures(1, &diffuse_map);
     }
 }
 
@@ -258,5 +265,52 @@ fn process_inputs(window: &mut Window, camera: &mut Camera, delta_time: f32) {
     }
     if window.get_key(Key::D) == Action::Press {
         camera.move_right(camera_speed);
+    }
+}
+
+fn load_texture(path: &str) -> Result<GLuint, String> {
+    // read image data
+    let img = image::open(&Path::new(path)).map_err(|e| format!("Could not load texture {}", e))?;
+
+    let format = match img.color() {
+        ColorType::RGB(_) => gl::RGB,
+        ColorType::RGBA(_) => gl::RGBA,
+        ColorType::Gray(_) => gl::DEPTH_COMPONENT,
+        ColorType::GrayA(_) => gl::DEPTH_STENCIL,
+        ColorType::BGR(_) => gl::BGR,
+        ColorType::BGRA(_) => gl::BGRA,
+        ColorType::Palette(_) => gl::DEPTH_COMPONENT,
+    };
+
+    unsafe {
+        // initialize texture
+        let mut texture = 0;
+        gl::GenTextures(1, &mut texture);
+
+        // set the texture wrapping parameters
+        // set texture wrapping to GL_REPEAT (default wrapping method)
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as GLint);
+        // set texture filtering parameters
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
+
+        // transfer image data
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            format as GLint,
+            img.width() as GLint,
+            img.height() as GLint,
+            0,
+            format,
+            gl::UNSIGNED_BYTE,
+            &img.raw_pixels()[0] as *const u8 as *const c_void,
+        );
+
+        // generate all mip map images for us
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        Ok(texture)
     }
 }
